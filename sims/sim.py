d55f15ebb757c255a544568648b82f25999ecfa9
maulik <maulik@cs.wisc.edu>
2018-09-21 11:34:37 -0500

    moving object included

--- sims/sim.py
+++ sims/sim.py
@@ -37,6 +37,12 @@ class Segment():
 	def setEnd(self, a,b):
 		self.end.set(a,b)
 
+	def setStartPoint(self, a):
+		self.start = a
+
+	def setEndPoint(self, a):
+		self.end = a
+
 	def getStart(self):
 		return self.start
 
@@ -46,6 +52,13 @@ class Segment():
 def distance(a,b):
 	return np.sqrt(np.sum(np.square((a-b).getCoords())))
 
+
+def movePoint(point, delta, vec):
+	coords = vec.getCoords()
+	coords = delta*coords
+	p = Point()
+	p.setCoords(coords)
+	return point+p
 
 laser = Point()
 camera = Point()
@@ -66,7 +79,7 @@ obj2.setEnd(11,5)
 
 
 # print(distance(pointLit,obj1))
-print(distance(laser,obj1))
+# print(distance(laser,obj1))
 
 
 eps = 1e-6
@@ -78,22 +91,59 @@ p = obj2.getStart()
 diff = obj2.getEnd()-obj2.getStart()
 
 
-print(obj2.getStart().getCoords())
-print(obj2.getEnd().getCoords())
-print(diff.getCoords())
-i=0
+# print(obj2.getStart().getCoords())
+# print(obj2.getEnd().getCoords())
+# print(diff.getCoords())
 
 
-def movePoint(point, delta, vec):
-	coords = vec.getCoords()
-	coords = delta*coords
-	p = Point()
-	p.setCoords(coords)
-	return point+p
+time=0
 
-print("done 1")
-while distance(p,obj2.getEnd()) > eps:
-	print(i,distance(p,pointSeen))
-	i=i+1
+count = 2
+wallPoints = np.asarray([Point() for x in xrange(count*count)])
 
-	p = movePoint(p,delta,diff)
+counter = -((count**2)/2)
+
+# init point coordinates of wallpoints being observed
+for point in wallPoints:
+	# point.set(counter/count, counter%count)
+	point.set(counter, 10)
+	print(counter, point.getCoords())
+	counter = counter + 1
+
+# while distance(p,obj2.getEnd()) > eps:
+# 	print(time,distance(p,pointSeen))
+# 	time=time+1
+
+# 	p = movePoint(p,delta,diff)
+
+totalTime=3
+deltaTime=1
+
+obj = Segment()
+obj.setStart(8,5)
+obj.setEnd(9,5)
+
+speed = Point()
+speed.set(0,0)
+
+def moveSegment(obj,speed):
+	obj.setStartPoint(obj.getStart() + speed)
+	obj.setEndPoint(obj.getEnd() + speed)
+
+delta= 0.1
+
+# outer loop (time) is only useful for moving objects
+for time in range(0,totalTime,deltaTime):
+	print("time",time)
+	# scanning the wall, one raster point at an instant
+	for wp in wallPoints:
+		print("wp",wp.getCoords())
+		pLit  = wp
+		pSeen = wp
+		p = obj.getStart()
+		diff = obj.getEnd()-obj.getStart()
+		while distance(p,obj.getEnd()) > eps:
+			print("hidden point observed", p.getCoords())
+			print("peak in time intensity curve at ", distance(pSeen,p))
+			p = movePoint(p,delta,diff)	
+	moveSegment(obj,speed)
